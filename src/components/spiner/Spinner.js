487b59da1580a23373babb6517a675410e0a5eca
Tianlikai <1024998526@qq.com>
2018-08-10 15:10:38 +0800

    添加 Spinner 组件

---
+++ src/components/spiner/Spinner.js
@@ -0,0 +1,11 @@
+import Component from '../Component'
+import src from './spinner.gif'
+
+export default class Spinner extends Component {
+    render() {
+        const style = this.props.style || {'display': 'block', 'width': '40px', margin: '50px auto'}
+        return (
+            <img style={style} src={src} />
+        )
+    }
+}
