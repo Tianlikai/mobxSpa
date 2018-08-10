3b29b5e080c4fb984b420c21ee0b45791092aaad
Tianlikai <1024998526@qq.com>
2018-08-10 15:20:04 +0800

    组件样式修改

--- src/layouts/Container.js
+++ src/layouts/Container.js
@@ -2,13 +2,17 @@ import Component from '../components/Component'
 
 const Container = config => WrappedComponent => class extends Component {
     render() {
+        const footer = {
+            color: 'gray',
+            fontSize: 18
+        }
         return (
             <div className='container'>
                 <div className='content'>
                     <div className='subTitle'>{config[0] || '-'} / <span>{config[1] || '-'}</span></div>
                     <WrappedComponent {...this.props} />
                 </div>
-                <div className='footer'>
+                <div className='footer' style={footer}>
                     power to go!
                 </div>
             </div>
