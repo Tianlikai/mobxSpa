f2bf0eb7f3e7ecc97f0e964ea57cb6e8f11af9b4
Tianlikai <1024998526@qq.com>
2018-08-10 14:59:00 +0800

    add 机构模拟模块

---
+++ src/mocker/orgLog.js
@@ -0,0 +1,69 @@
+const Mock = require('mockjs')
+
+class OrgLog {
+    constructor(initialLength) {
+        this.initialLength = initialLength || 105
+        let cached = []
+        for (let i = 0; i < this.initialLength; ++i) {
+            let mock = Mock.mock({
+                'item': {
+                    'id|+1': 1300000 + i,
+                    'operationType|1': [0, 1, 2, 3, 4, 5],
+                    'orgName': Mock.Random.cword(4),
+                    'operationDescription': Mock.Random.csentence(),
+                    'username': Mock.Random.cname(),
+                    'createdAtFormat': Mock.Random.datetime()
+                }
+            })
+            cached.push(mock.item)
+        }
+        this.cached = cached
+    }
+
+    /**
+     * @param {num: 页} p
+     * @param {num: 行数} i
+     * @param {num: 总数} max
+     */
+    getStartAndStop(p, i, max) {
+        let start = (p - 1) * 10
+        let end = start + 10
+        if (end > max) end = max
+        return {start, end}
+    }
+
+    /**
+     * 获取机构日志列表
+     * @param {string: 搜索条件} name
+     * @param {number: 页码} pageNo
+     * @param {number: 每页长度} itemsPerPage
+     * @param {number: 该条记录状态} state
+     */
+    getLog(name, pageNo, itemsPerPage, state) {
+        let copyCached = this.cached.slice()
+        state = state ? state + '' : null
+        name = name ? name + '' : null
+        if (name) {
+            copyCached = copyCached.filter(element => {
+                return element.name.indexOf(name) >= 0 || (element.id + '').indexOf(name) >= 0 || (element.mobile + '').indexOf(name) >= 0
+            })
+        }
+        if (state) {
+            copyCached = copyCached.filter(element => {
+                return element.countState + '' === state
+            })
+        }
+        let len = copyCached.length
+        let items = []
+        let {start, end} = this.getStartAndStop(pageNo, itemsPerPage, len)
+        while (start < end) {
+            items.push(copyCached[start])
+            ++start
+        }
+        return {items, count: len}
+    }
+}
+
+const orgLog = new OrgLog()
+
+module.exports = orgLog
