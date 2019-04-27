0a84485d7a79f2ce6b5203acc6bb264aec2d845a
Tianlikai <1024998526@qq.com>
2019-04-27 21:27:20 +0800

    简化webpack配置

--- build/index.js
+++ build/index.js
@@ -1,6 +1,11 @@
-/* eslint-disable */
-if (process.env.NODE_ENV === 'prod') {
-  module.exports = require('./webpack.prod.config');
-} else if (process.env.NODE_ENV === 'dev') {
-  module.exports = require('./webpack.dev.config');
-}
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base.config.js');
+const devConfig = require('./webpack.dev.config');
+const prodConfig = require('./webpack.prod.config');
+
+module.exports = () => {
+  if (process.env.NODE_ENV === 'prod') {
+    return merge(baseConfig, prodConfig);
+  }
+  return merge(baseConfig, devConfig);
+};
