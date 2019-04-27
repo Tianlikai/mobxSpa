07d6d4464bdca990d46ba74812c7c19c50d946af
Tianlikai <1024998526@qq.com>
2019-04-27 21:14:28 +0800

    更新webpack.dll.config.js

--- build/webpack.dll.config.js
+++ build/webpack.dll.config.js
@@ -1,81 +1,39 @@
-/* eslint-disable */
-
 const path = require('path');
-
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const rootDir = path.resolve(__dirname);
-const staticDir = path.resolve(rootDir, '../src/static');
+const staticDir = path.resolve(rootDir, '../dll');
 
 module.exports = {
   mode: 'production',
+  performance: {
+    hints: false,
+  },
   entry: {
     vendor: [
       'antd',
-      'antd/dist/antd.min.css',
       'echarts',
+      'react-helmet',
+      'react-loadable',
+    ],
+    react: [
       'react',
       'react-dom',
-      'react-helmet',
       'react-router-dom',
-      'react-loadable',
-      'babel-polyfill',
       'prop-types',
       'mobx',
       'mobx-react',
     ],
   },
   output: {
-    path: path.resolve(staticDir, 'dll'),
+    path: path.resolve(staticDir),
     filename: 'dll.[name].[chunkhash].js',
     library: 'dll_[name]_[chunkhash]',
   },
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true,
-      }),
-      new OptimizeCSSAssetsPlugin({}),
-    ],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          { loader: 'css-loader' },
-          { loader: 'postcss-loader' },
-          { loader: 'sass-loader' },
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 8192,
-        },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        loader: 'file-loader',
-      },
-    ],
-  },
   plugins: [
     new webpack.DllPlugin({
-      path: path.resolve(staticDir, 'dll', 'manifest.dll.json'),
       name: 'dll_[name]_[chunkhash]',
-      context: rootDir,
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'dll.[name].[chunkhash].css',
-      allChunks: true,
+      path: path.resolve(staticDir, 'manifest.dll.json'),
     }),
   ],
 };
